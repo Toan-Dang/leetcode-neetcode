cdce0ea807fd58a8f3f207b56fdc40a5e527984e
Đặng Phúc Toàn <65716782+Toan-Dang@users.noreply.github.com>
2020-05-08 15:16:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        return powl(x,n);
+    double myPow(double a, int n) {
+        double s;
+    if (n == 0)
+        return 1;
+    s = myPow(a, n / 2);
+    if (n % 2 == 0) 
+        return s * s;
+    else
+    {
+        if (n > 0)
+            return a * s * s;
+        else
+            return (s * s) / a;
+    }
     }
 };
