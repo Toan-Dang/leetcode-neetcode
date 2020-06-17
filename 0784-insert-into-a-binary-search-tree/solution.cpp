e3e1f2ec3afeecab3930147dd884f38b4bd36c01
Đặng Phúc Toàn <65716782+Toan-Dang@users.noreply.github.com>
2020-06-17 09:15:28 +0000

    Sync LeetCode submission Runtime - 96 ms (0.19%), Memory - 38.5 MB (99.98%)

--- 0784-insert-into-a-binary-search-tree/solution.cpp
+++ 0784-insert-into-a-binary-search-tree/solution.cpp
@@ -4,22 +4,19 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    TreeNode* insertIntoBST(TreeNode* root, int val) {
-        if(root == NULL){
-            TreeNode*p= new TreeNode(val);
-            return p;
-        }
-        if(root->val < val){
-           root->right= insertIntoBST(root->right, val);
-        }else{
-            root->left = insertIntoBST(root->left,val);
-        }
-            return root;
-    }
-
+    	TreeNode *insertIntoBST(TreeNode *root, int val)
+	{
+		TreeNode **cur = &root;
+		while( *cur )
+			cur = (val > (*cur)->val) ? &(*cur)->right : &(*cur)->left;
+		*cur = new TreeNode(val);
+		return root;
+	}
 };
