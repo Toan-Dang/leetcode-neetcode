591f190fd995b3e3206380226640abe6b9ac92d4
Đặng Phúc Toàn <65716782+Toan-Dang@users.noreply.github.com>
2020-07-06 04:04:55 +0000

    Sync LeetCode submission Runtime - 48 ms (8.38%), Memory - 8.4 MB (99.99%)

---
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+ int lengthOfLongestSubstring(string s) {
+     ios_base::sync_with_stdio(false);
+   map<char, int> charMap;
+        int start = -1;
+        int maxLen = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            if (charMap.count(s[i]) != 0) {
+                start = max(start, charMap[s[i]]);
+            }
+            charMap[s[i]] = i;
+            maxLen = max(maxLen, i-start);
+        }
+        
+        return maxLen;
+    }
+ 
+};
